736eb43d10e8eef08dae91374c27508fd192658e
mrmcmva <vsevolodmartynyuk@yandex.ru>
2025-09-25 20:00:38 +0300

    Current project was completed

---
+++ Course_ThirdProject/crud/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm, CharField, IntegerField
+from .models import Person
+
+
+class PersonForm(ModelForm):
+    name = CharField(label='Введите имя')
+    age = IntegerField(label='Введите возраст')
+
+    class Meta:
+        model = Person
+        fields = ['name', 'age']
